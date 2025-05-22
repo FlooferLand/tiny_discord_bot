433e2cbb414e280605edc343248cea47e1f3bce1
FlooferLand <76737186+FlooferLand@users.noreply.github.com>
2025-05-22 03:32:37 +0300

    Add git repo mention

--- src/commands/info.rs
+++ src/commands/info.rs
@@ -2,6 +2,6 @@ use crate::{Context, Error};
 
 #[poise::command(slash_command)]
 pub async fn info(ctx: Context<'_>) -> Result<(), Error> {
-    ctx.reply("Silly bot made by FlooferLand").await.unwrap();
+    ctx.reply("Silly bot made by FlooferLand!\nRepo: <https://github.com/FlooferLand/tiny_discord_bot>").await?;
     Ok(())
 }

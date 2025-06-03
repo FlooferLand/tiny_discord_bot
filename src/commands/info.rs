7796a8dae0a1ad99dd143b100deaad8672323d83
FlooferLand <76737186+FlooferLand@users.noreply.github.com>
2025-06-03 06:19:18 +0300

    Bump version & add indoc string formatting

--- src/commands/info.rs
+++ src/commands/info.rs
@@ -1,9 +1,18 @@
-use crate::{Context, BotError};
 use crate::error::BotErrorExt;
+use crate::{BotError, Context};
+use indoc::formatdoc;
 
 /// Some information about teh bot!
-#[poise::command(slash_command)]
+#[poise::command(slash_command, rename="info")]
 pub async fn bot_info(ctx: Context<'_>) -> Result<(), BotError> {
-    ctx.reply("Silly bot made by FlooferLand!\nRepo: <https://github.com/FlooferLand/tiny_discord_bot>").await.bot_err()?;
+    let message = formatdoc!(
+        r#"
+            Silly bot made by FlooferLand!
+            Repo: <https://github.com/FlooferLand/tiny_discord_bot>
+            Version: `{Version}`
+        "#,
+        Version = env!("CARGO_PKG_VERSION")
+    );
+    ctx.reply(message).await.bot_err()?;
     Ok(())
 }
